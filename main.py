216479dc0700e86114fdf4a1370b789916a964ac
sarvente42 <katianechyporuk07@gmail.com>
2022-11-10 03:08:03 +0200

    done 2nd ex

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+a = int(input('number 1->>'))
+b = int(input('number 2->>'))
+c = int(input('number 3->>'))
+action = input('action->>')
+if action == 'max':
+    if a > b and a > c:
+        print(f'{a} is max')
+    elif b > a and b > c:
+        print(f'{b} is max')
+    elif c > a and c > b:
+        print(f'{c} is max')
 
+elif action == 'min':
+    if a < b and a < c:
+        print(f'{a} is min')
+    elif b < a and b < c:
+        print(f'{b} is min')
+    elif c < a and c < b:
+        print(f'{c} is min')
+
+elif action == 'avg':
+    print((a + b + c)/3)
+elif a == b == c:
+    print('All numbers are equal')
+else:
+    print('The action is not found')
