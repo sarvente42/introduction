aea44d95c5e8215824e5e0d23905cd1667df84c5
sarvente42 <katianechyporuk07@gmail.com>
2022-11-10 02:54:20 +0200

    done 1st ex

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+a = int(input("number 1->>"))
+b = int(input("number 2->>"))
+c = int(input("number 3->>"))
+action = input('action->>')
+if action == '+':
+    print(a + b + c)
+elif action == '*':
+    print(a * b * c)
+else:
+    print("Action is not found")
