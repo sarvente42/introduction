aa543e71d86507c3a17f29780410fd74fcbf822a
sarvente42 <katianechyporuk07@gmail.com>
2022-11-10 02:48:02 +0200

    done 5th ex

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-
+a = int(input("number 1->>"))
+b = int(input('number 2->>'))
+action = input("action->>")
+if action == "+":
+    print(a + b)
+elif action == "-":
+    print(a - b)
+elif action == "avg":
+    print((a+b)/2)
+elif action == "*":
+    print(a * b)
+else:
+    print("The action is not found")
