79b0c8a4807650b3173e9f63af251c2fa8267b28
sarvente42 <katianechyporuk07@gmail.com>
2022-11-10 03:14:28 +0200

    done 3rd ex

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+a = int(input('number->>'))
+action = input('action->>')
+if action == 'mi':
+    print(a * 0.000621371196)
+elif action == 'in':
+    print(a * 39.37)
+elif action == 'yd':
+    print(a * 1.0936)
+else:
+    print("Action is not found")
