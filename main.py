4da8ca4d6e540be2b7dce78c027163cb9ae953cb
sarvente42 <katianechyporuk07@gmail.com>
2022-11-18 01:49:27 +0200

    done 2nd ex

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+try:
+    diameter = float(input('Diameter of the circle->>'))
+    action = input('action->>')
+    r = float(diameter / 2)
+
+    if action == 'square':
+        print(float(3.14 * (r ** 2)))
+    elif action == 'perimeter':
+        print(float(3.14 * diameter))
+    else:
+        print('Error!')
+except ValueError:
+    print('Error! Wrong type of function')
+
 
