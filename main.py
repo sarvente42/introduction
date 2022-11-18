2c08f65de19d106fcef3ece84cb73b6fe73a88c3
sarvente42 <katianechyporuk07@gmail.com>
2022-11-18 02:32:22 +0200

    done 3rd ex

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+try:
+    PriceOfOne = int(input('Price->>'))
+    Amount = int(input('The amount of consoles->>'))
+    DiscountInPercentage = float(input('The percentage of discount->>'))
+    action = input('action->>')
+    Discount = float(DiscountInPercentage / 100)
+    PriceOfFew = (PriceOfOne * Amount)
 
+    if action == 'sum of the order without discount':
+        print(PriceOfFew)
+    elif action == 'sum of the order with discount':
+        Discount1 = (PriceOfFew * Discount)
+        print(PriceOfFew - Discount1)
+    elif action == 'price of one console with discount':
+        Discount2 = (PriceOfOne * Discount)
+        print(PriceOfOne - Discount2)
+except ValueError as vl_er:
+    print('Error! Wrong type of function,', vl_er)
