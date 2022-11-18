7943cb1a94febea014ecbc44c065db90df935c72
sarvente42 <katianechyporuk07@gmail.com>
2022-11-18 13:02:55 +0200

    completed 7 strings

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+FileSize = float(input('The size of file in gigabytes->>'))
+SpeedInBitForSec = float(input('The speed of the internet connection in bit/sec->>'))
+SpeedInGbForSec = float(SpeedInBitForSec * 1.25 * 10 ** -10)
+TimeInSec = (FileSize / SpeedInGbForSec)
+action = input('action->>')
 
+if action == 'hours':
