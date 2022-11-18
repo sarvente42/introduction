37532e126f3c2af863f999d7f6ea9c37a177e144
sarvente42 <katianechyporuk07@gmail.com>
2022-11-18 21:32:04 +0200

    done 5th ex

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+try:
+    hours = int(input('Hours->>'))
+
+    if hours < 0:
+        raise Exception('Hours are less than Zero')
+    elif hours > 24:
+        raise Exception('Hours are more than Twenty Four')
+ #я не знаю чому тут не правильно працюють raise-и
+
+    elif 0 <= hours < 6:
+        print('Good Night')
+    elif 6 <= hours < 13:
+        print('Good Morning')
+    elif 13 <= hours < 17:
+        print('Good Day')
+    elif 17 <= hours <= 23:
+        print('Good Evening')
+
+except ValueError as vl_er:
+    print('Error! Wrong type of function,', vl_er)
+
+
 
