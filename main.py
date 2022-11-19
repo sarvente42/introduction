91b946db56e20c5287b9fde2492cea745bd1c3c2
sarvente42 <katianechyporuk07@gmail.com>
2022-11-19 21:18:22 +0200

    done 3rd ex

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+try:
+    number = float(input('Your number->> '))
+
+    if number > 0:
+        print('Your number is positive')
+    elif number < 0:
+        print('Your number is negative')
+    elif number == 0:
+        print('Your number is equal to Zero')
+except ValueError as vl_er:
+    print('Error!Wrong type of function,', vl_er)
 
