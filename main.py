8fb273b954e5d4bbc829280f379a50e7010231b9
sarvente42 <katianechyporuk07@gmail.com>
2022-11-19 21:26:02 +0200

    done 4th ex

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+try:
+    number1 = float(input('First number->> '))
+    number2 = float(input('Second number->>'))
 
+    if number1 > number2:
+        print(number2, number1)
+    elif number2 > number1:
+        print(number1, number2)
+    else:
+        print('First and second numbers are equal')
+except ValueError as vl_er:
+    print('Error! Wrong type of function,', vl_er)
