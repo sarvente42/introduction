6d84ba8afafa0f2c173df09dc0c3e9bf0932ef09
sarvente42 <katianechyporuk07@gmail.com>
2022-11-22 23:55:30 +0200

    done 2nd ex

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+try:
+    number = int(input('number->>'))
+    st = int(input('st->>'))
+    if st < 0 or st > 7:
+        raise Exception('St is less then 0 or more then 7')
+    else:
+        print(number ** st)
 
+except ValueError as vl_er:
+    print('Error,', vl_er)
+except Exception as ex:
+    print(f'Error:{ex}')
