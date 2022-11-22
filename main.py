5bcf1bb34dd38305e2bec09288d378586034d4e7
sarvente42 <katianechyporuk07@gmail.com>
2022-11-22 23:54:59 +0200

    done 1st ex

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+try:
+    number = int(input('Your number->>'))
+    if number < 0 or number > 100:
+        raise Exception('Your number is less then 0 or more then 100')
+    elif number % 3 == 0 and number / 5 != 3:
+        print('Fizz')
+    elif number % 5 == 0 and number / 3 != 5:
+        print('Buzz')
+    elif number % 3 == 0 and number % 5 == 0:
+        print('Fizz Buzz')
+    elif number % 5 != 0 and number % 3 != 0:
 
+        print(number)
+except ValueError as vl_er:
+    print('Error,', vl_er)
+except Exception as ex:
+    print(f'Error:{ex}')
