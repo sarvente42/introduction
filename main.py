5b3714ab3186c8366c841d5b63a9983e974b7e41
sarvente42 <katianechyporuk07@gmail.com>
2022-12-06 18:14:35 +0200

    done 2nd ex

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+try:
+    a = int(input('begin->>'))
+    b = int(input('end->>'))
+    counter = int(0)
+    if a and b == 0:
+        raise Exception
+    for i in reversed(range(a, b)):
+        print(i, end='\t')
+    for i in range(a, b):
+        print(i, end='\t')
+    for i in range(a, b):
+        if i % 7 == 0:
+            print(i, end='\t')
+    for i in range(a, b):
+        if i % 5 == 0:
+            counter = counter + 1
+    print(counter)
+
+
+
 
