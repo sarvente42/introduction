4bab6c28e1f8508427816271de783654b68793dc
sarvente42 <katianechyporuk07@gmail.com>
2022-12-06 20:05:55 +0200

    done 3rd ex

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+a = int(input('begin->>'))
+b = int(input('end->>'))
+
+for i in range(a+1, b):
+    if i % 3 == 0:
+        print('fizz', end=' ')
+    if i % 5 == 0:
+        print('buzz', end='')
+    print()
 
