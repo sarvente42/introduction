0d39390943ab074b793615f6d378e0f66fefcadd
sarvente42 <katianechyporuk07@gmail.com>
2022-12-06 18:34:43 +0200

    update 2nd ex

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ try:
     a = int(input('begin->>'))
     b = int(input('end->>'))
     counter = int(0)
-    if a and b == 0:
-        raise Exception
+    if a == 0 and b == 0:
+        raise Exception('Both of numbers equals Zero')
     for i in reversed(range(a, b)):
         print(i, end='\t')
     for i in range(a, b):
@@ -15,6 +15,12 @@ try:
         if i % 5 == 0:
             counter = counter + 1
     print(counter)
+
+except ValueError as vl_er:
+    print('Error!', vl_er)
+except Exception as ex:
+    print('Error!', ex)
+    print(f'Name: {ex.__class__.__name__}')
 
 
 
