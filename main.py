c836a7e6c499be0bfe04832afece6637937d2e31
sarvente42 <katianechyporuk07@gmail.com>
2022-12-06 16:54:06 +0200

    done 5th ex

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+try:
+    a = int(input('begin->>'))
+    b = int(input('end->>'))
+    if a > b:
+        a, b = b, a
 
+    for i in range(a, b):
+        if i % 2 != 0:
+            print(i, end='  ')
+except ValueError as vl_er:
+    print('Error!', vl_er)
