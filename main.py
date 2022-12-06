fb4d35a0e006ee4137550d782bf3ad73f5a28227
sarvente42 <katianechyporuk07@gmail.com>
2022-12-06 16:38:26 +0200

    done 4th ex

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 try:
     a = int(input('begin->>'))
     b = int(input('end->>'))
-    for i in range(a, b):
+    for i in reversed(range(a+1, b)):
+        print(i, end='  ')
+except ValueError as vl_er:
+    print('Error!', vl_er)
+
