990fd2a2d76802ada66cffc25abbc45b4c3847c1
sarvente42 <katianechyporuk07@gmail.com>
2022-12-06 15:54:09 +0200

    done 1st ex with errors

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-a = int(input('begin->>'))
-b = int(input('end->>'))
+try:
+    a = int(input('begin->>'))
+    b = int(input('end->>'))
 
-for item in range(a,  b):
-    print(item, end='  ')
+    for item in range(a,  b):
+        print(item, end='  ')
+except ValueError as vl_er:
+    print('Error!', vl_er)
