3c432106273ae2de7f65bb5227d5d32bfcf26437
sarvente42 <katianechyporuk07@gmail.com>
2023-02-06 23:49:02 +0200

    redone 1st ex

--- main.py
+++ main.py
@@ -4,29 +4,29 @@ try:
     function = input()
     sum = 0
     counter = 0
-    for i in range(begin, end+1):
-        if function == 'even numbers':
+    if function == 'even numbers':
+        for i in range(begin, end+1):
             if i % 2 == 0:
                 sum += i
                 counter += 1
-                print(sum)
-                avg = sum / counter
-                print(avg)
-        elif function == 'buzz numbers':
+        print(sum)
+        print(sum / counter)
+    if function == 'buzz numbers':
+        for i in range(begin, end+1):
             if i % 2 != 0:
                 sum += i
-                print(sum)
                 counter += 1
-                print(sum)
-                avg = sum / counter
-        elif function == 'multiple of 9':
+        print(sum)
+        print(sum / counter)
+    if function == 'multiple of 9':
+        for i in range(begin, end+1):
             if i % 9 == 0:
                 sum += i
-                print(sum)
                 counter += 1
-                print(sum)
-                avg = sum / counter
-
+        print(sum)
+        print(sum / counter)
 
 except ArithmeticError as ar_vl:
     print(f'Error', ar_vl)
+except ValueError as vl_er:
+    print(vl_er)
