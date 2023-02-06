c7918ce02b148ab1c6464e02a826404737bc3c08
sarvente42 <katianechyporuk07@gmail.com>
2023-02-06 22:36:47 +0200

    done 2nd ex

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+try:
+    a = int(input('length->>'))
+    b = input('sign->>')
+    if a == 0 and b == 0:
+        raise Exception("Both numbers equals Zero")
+    for i in range(0, a):
+        print(b, end='\n')
 
+except ValueError as vl_er:
+    print('Error!', vl_er)
+except Exception as ex:
+    print('Error!', ex)
