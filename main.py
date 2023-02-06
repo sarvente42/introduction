9a6d18bf817968ef072353f7e2c4d08834b1fa8b
sarvente42 <katianechyporuk07@gmail.com>
2023-02-07 00:09:35 +0200

    done 4th ex

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-
+try:
+    number = 0
+    while True:
+        number = int(input('number->'))
+        if number == 7:
+            break
+        elif number > 0:
+            print('number is positive')
+        elif number < 0:
+            print('number is negative')
+        elif number == 0:
+            print('number is equal to zero')
+except ValueError as vl_er:
+    print(vl_er)
